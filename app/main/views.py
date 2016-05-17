dd033c1ea20cb10a5664f5abf8b589f12ac5db46
yejiongtao <1183872353@qq.com>
2016-05-17 11:36:16 -0700

    add general views

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,22 @@
 from . import main
-from flask import render_template
+from flask import render_template, redirect, session, url_for
 
 
 @main.route('/')
 def index():
-    return render_template('hello.html')
+    return render_template('index.html')
+
+
+@main.route('/policy', methods=['GET','POST'])
+def policy():
+    return render_template('policy.html')
+
+
+@main.route('/user', methods=['GET','POST'])
+def user():
+    return render_template('user.html')
+
+
+@main.route('/notification')
+def notification():
+    return render_template('notification.html')
